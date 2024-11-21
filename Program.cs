dc881c84595e41756c7c0e244efc0bf34542a509
IkerValentin <iker.valentin@cirvianum.cat>
2024-11-21 09:40:19 +0100

    Modificació dels dos vehicles una altre vegada

--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@ class Program
     static void Main(string[] args)
     {
         // Crear un objecte Vehicle
-        Vehicle vehicle1 = new Vehicle("Kia", "Kia", 2000, 120.0);
+        Vehicle vehicle1 = new Vehicle("Subaru", "Kla", 2000, 120.0);
 
         // Mostrar la informació del vehicle
         vehicle1.MostrarInformacio();
 
         // Crear un altre objecte Vehicle
-        Vehicle vehicle2 = new Vehicle("Kia", "Kia", 1995, 240.0);
+        Vehicle vehicle2 = new Vehicle("Volvobagen", "Kai", 1995, 240.0);
         
         // Mostrar la informació del segon vehicle
         vehicle2.MostrarInformacio();
