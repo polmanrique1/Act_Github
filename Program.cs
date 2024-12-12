d162075b03a3b369f1bcd030010120e9f1d77ebc
IkerValentin <iker.valentin@cirvianum.cat>
2024-12-12 08:56:05 +0100

    Tot arreglat, disculpeu les molesties

--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@ class Program
     static void Main(string[] args)
     {
         // Crear un objecte Vehicle
-        Vehicle vehicle1 = new Vehicle("Subaru", "Kla", 2000, 120.0);
+        Cotxe vehicle1 = new Cotxe("Subaru", "Kla", 2000, 120.0, 4);
 
         // Mostrar la informació del vehicle
         vehicle1.MostrarInformacio();
 
         // Crear un altre objecte Vehicle
-        Vehicle vehicle2 = new Vehicle("Volvobagen", "Kai", 1995, 240.0);
+        Cotxe vehicle2 = new Cotxe("Volvobagen", "Kai", 1995, 240.0, 4);
         
         // Mostrar la informació del segon vehicle
         vehicle2.MostrarInformacio();
