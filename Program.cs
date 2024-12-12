3891a1103f50866df0b48ab9c1c2c82924f56a71
IkerValentin <iker.valentin@cirvianum.cat>
2024-12-12 09:37:08 +0100

    Primers fitxers del Merge

--- Program.cs
+++ Program.cs
@@ -5,16 +5,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Crear un objecte Vehicle
-        Cotxe vehicle1 = new Cotxe("Subaru", "Kla", 2000, 120.0, 4);
+         // Crear llistes per a cada tipus de vehicle
+        List<Vaixell> vaixells = new List<Vaixell>
+        {
+            new Vaixell("Explorer", "OceanX", 2015, 45, 12000),
+            new Vaixell("Voyager", "BlueWave", 2020, 60, 15000)
+        };
 
-        // Mostrar la informació del vehicle
-        vehicle1.MostrarInformacio();
+        List<Cotxe> cotxes = new List<Cotxe>
+        {
+            new Cotxe("Model S", "Tesla", 2022, 250, 4),
+            new Cotxe("Civic", "Honda", 2019, 220, 4)
+        };
 
-        // Crear un altre objecte Vehicle
-        Cotxe vehicle2 = new Cotxe("Volvobagen", "Kai", 1995, 240.0, 4);
-        
-        // Mostrar la informació del segon vehicle
-        vehicle2.MostrarInformacio();
+        List<Camio> camions = new List<Camio>
+        {
+            new Camio("Actros", "Mercedes", 2021, 120, 18000),
+            new Camio("FH16", "Volvo", 2018, 110, 25000)
+        };
+
+        // Mostrar informació de cada tipus de vehicle
+        Console.WriteLine("Llista de Vaixells:");
+        foreach (var vaixell in vaixells)
+        {
+            vaixell.MostrarInformacio();
+        }
+
+        Console.WriteLine("\nLlista de Cotxes:");
+        foreach (var cotxe in cotxes)
+        {
+            cotxe.MostrarInformacio();
+        }
+
+        Console.WriteLine("\nLlista de Camions:");
+        foreach (var camio in camions)
+        {
+            camio.MostrarInformacio();
+        }
     }
 }
