801e58ef3c1f262567f6ae358522b57edcadc685
polmanrique1 <polmanriqueortiz@gmail.com>
2025-02-13 08:56:19 +0100

    canvi cotxe a Koenigsegg Program.cs

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ class Program
 
         List<Cotxe> cotxes = new List<Cotxe>
         {
-            new Cotxe("Act", "Kia", 2022, 250, 4),
+            new Cotxe("Regera.", "Koenigsegg", 2022, 250, 4),
             new Cotxe("Civic", "Honda", 2019, 220, 4)
         };
 
